3314f9a49fd693610fe7a4d84b58a56fad8fec17
Danila <danilahreno@gmail.com>
2023-01-29 21:34:20 +0300

    Добавьте файлы проекта.

---
+++ Fac/Fac.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int long z;
+    while (true) {
+        cin >> z;
+        if (z == 0) {
+            break;
+        }
+        else {
+            factorial(z);
+        }
+    }
+}
+
+void factorial(int long x) {
+    int long fac;
+
+    fac = 1;
+    for (int i = 1; i <= x; i++) {
+        fac *= i;
+    }
+    cout << fac;
+}
