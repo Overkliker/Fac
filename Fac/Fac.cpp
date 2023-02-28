8489b9a6f198eb5589d345fe32626fcc73f098e4
kliker <danilahreno@gmail.com>
2023-02-28 22:04:59 +0300

    Great

--- Fac/Fac.cpp
+++ Fac/Fac.cpp
@@ -1,18 +1,43 @@
 ﻿#include <iostream>
 using namespace std;
 
+void factorial(int long x);
+bool isprime(int long num);
+
 int main()
 {
-    int long z;
+    setlocale(LC_ALL, "Russian");
+    int long z, a;
+    string end;
+
+    a = 0;
     while (true) {
         cin >> z;
-        if (z == 0) {
+        factorial(z);
+        if (isprime(z) == true) {
+            a += z;
+        }
+        cout << isprime(z);
+
+        cout << "Вы хотите завершить работу?";
+        cin >> end;
+
+        if (end == "end") {
+            cout << a;
             break;
         }
-        else {
-            factorial(z);
-        }
     }
+}
+
+bool isprime(int long num)
+{
+
+    if ((num * num) % 24 == 1)
+    {
+        return true;
+    }
+    return false;
+
 }
 
 void factorial(int long x) {
@@ -22,5 +47,8 @@ void factorial(int long x) {
     for (int i = 1; i <= x; i++) {
         fac *= i;
     }
-    cout << fac;
+    cout << "Факториал: ";
+    cout << fac << "\n";
+
+
 }
